710faf0d185ed4fa65f7a3a5f9ed967fc8d0f289
Josh Thompson <thompsonjoshd@gmail.com>
2021-05-04 11:25:28 -0400

    Add Rulers::Application#call

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,6 +1,13 @@
 require "rulers/version"
 
 module Rulers
+  class Application
+    def call(env)
+      [200, {'Content-Type' => 'text/html'},
+      ["Hello from RoRulers"]]
+    end
+  end
+  
   class Error < StandardError; end
   # Your code goes here...
 end
